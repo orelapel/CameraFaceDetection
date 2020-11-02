04f7e654e332b71c553e4fe438663402a64f4743
orell <orell@DESKTOP-KOE6OVD>
2020-11-02 10:49:21 +0200

    update recognize loop

--- app/src/main/java/com/example/camerafacedetection/MainActivity.java
+++ app/src/main/java/com/example/camerafacedetection/MainActivity.java
@@ -1,186 +1,133 @@
 package com.example.camerafacedetection;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Point;
-import android.graphics.Rect;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.PointF;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.camerafacedetection.Helper.GraphicOverlay;
-import com.example.camerafacedetection.Helper.RectOverlay;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.face.FirebaseVisionFace;
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
-import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
-import com.wonderkiln.camerakit.CameraKit;
-import com.wonderkiln.camerakit.CameraKitError;
-import com.wonderkiln.camerakit.CameraKitEvent;
-import com.wonderkiln.camerakit.CameraKitEventListener;
-import com.wonderkiln.camerakit.CameraKitImage;
-import com.wonderkiln.camerakit.CameraKitVideo;
-import com.wonderkiln.camerakit.CameraView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
-import dmax.dialog.SpotsDialog;
 
 public class MainActivity extends AppCompatActivity {
     private static final String ACTION_USB_PERMISSION =
             "com.android.example.USB_PERMISSION";
     UsbSerialPort port;
 
-    private Button faceDetectButton;
     private Button startButton;
     private Button changeButton;
+    boolean shouldStop=false;
+
+    private static final int PERMISSION_REQUESTS = 1;
+    private static final String TAG = "LivePreviewActivity";
+    private CameraSource cameraSource = null;
+    private CameraSourcePreview preview;
     private GraphicOverlay graphicOverlay;
-    private CameraView cameraView;
-    AlertDialog alertDialog;
+    private FaceDetectionProcessor faceDetectionProcessor;
 
     double deltaX;
     double deltaY;
     double L;
-    Point midFrameCam;
-    Point midRectFace;
+    PointF midFrameCam;
+    PointF midRectFace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        faceDetectButton = findViewById(R.id.detect_face_btn);
-        startButton = findViewById(R.id.start);
         changeButton = findViewById(R.id.change_cam);
-        graphicOverlay = findViewById(R.id.graphic_overlay);
-        cameraView = findViewById(R.id.camera_view);
-        midFrameCam = new Point(cameraView.getWidth()/2,cameraView.getHeight()/2);
+        preview = (CameraSourcePreview) findViewById(R.id.firePreview);
+        graphicOverlay = (GraphicOverlay) findViewById(R.id.fireFaceOverlay);
 
-        alertDialog = new SpotsDialog.Builder()
-                .setContext(this)
-                .setMessage("Please wait, Processing...")
-                .setCancelable(false)
-                .build();
-
-        faceDetectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                cameraView.start();
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        cameraView.captureImage();
-                        handler.postDelayed(this, 1);
-                    }
-                }, 0);  //the time is in miliseconds
-//                graphicOverlay.clear();
-            }
-        });
-
-        cameraView.addCameraKitListener(new CameraKitEventListener() {
-            @Override
-            public void onEvent(CameraKitEvent cameraKitEvent) {
-
-            }
-
-            @Override
-            public void onError(CameraKitError cameraKitError) {
-
-            }
-
-            @Override
-            public void onImage(CameraKitImage cameraKitImage) {
-                //alertDialog.show();
-                Bitmap bitmap = cameraKitImage.getBitmap();
-                bitmap = Bitmap.createScaledBitmap(bitmap, cameraView.getWidth(),
-                        cameraView.getHeight(), false);
-                //cameraView.stop();
-
-                processFaceDetection(bitmap);
-            }
-
-            @Override
-            public void onVideo(CameraKitVideo cameraKitVideo) {
-
-            }
-        });
-
-        //faceDetectButton.setEnabled(true);
-    }
-
-    private void processFaceDetection(Bitmap bitmap) {
-        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-
-        FirebaseVisionFaceDetectorOptions firebaseVisionFaceDetectorOptions
-                = new FirebaseVisionFaceDetectorOptions.Builder()
-                .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
-                .enableTracking().build();
-
-        FirebaseVisionFaceDetector firebaseVisionFaceDetector = FirebaseVision.getInstance()
-                .getVisionFaceDetector(firebaseVisionFaceDetectorOptions);
-        firebaseVisionFaceDetector.detectInImage(firebaseVisionImage)
-                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
-                    @Override
-                    public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
-                        getFaceResults(firebaseVisionFaces);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this, "Error: " + e.getMessage()
-                        , Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private void getFaceResults(List<FirebaseVisionFace> firebaseVisionFaces) {
-        int counter = 0;
-        for (FirebaseVisionFace face : firebaseVisionFaces) {
-            Rect rect = face.getBoundingBox();
-            RectOverlay rectOverlay = new RectOverlay(graphicOverlay, rect);
-
-            graphicOverlay.clear();
-            graphicOverlay.add(rectOverlay);
-            midRectFace = new Point(rect.width()/2, rect.height()/2);
-            L = rect.width() * rect.height();
-            //sendValues(k1ToDouble, k2ToDouble, k3ToDouble, cToDouble, dToDouble);
-
-            counter += 1;
+        if (allPermissionsGranted()) {
+            createCameraSource();
+            startCameraSource();
+        } else {
+            getRuntimePermissions();
         }
-        alertDialog.dismiss();
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    private void startCameraSource() {
+        if (cameraSource != null) {
+            try {
+
+                if (preview == null) {
+                    Log.d(TAG, "resume: Preview is null");
+                }
+                if (graphicOverlay == null) {
+                    Log.d(TAG, "resume: graphOverlay is null");
+                }
+                preview.start(cameraSource, graphicOverlay);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                cameraSource.release();
+                cameraSource = null;
+            }
+        }
+    }
+
+    private void createCameraSource() {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = new CameraSource(this, graphicOverlay);
+        }
+        faceDetectionProcessor = new FaceDetectionProcessor();
+        cameraSource.setMachineLearningFrameProcessor(faceDetectionProcessor);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        cameraView.stop();
+        preview.stop();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        cameraView.start();
+        startCameraSource();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (cameraSource != null) {
+            cameraSource.release();
+        }
+        if (!shouldStop) {
+            shouldStop = true;
+            try {
+                port.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static double tryParseToDouble(String strNum) {
@@ -205,26 +152,53 @@ public class MainActivity extends AppCompatActivity {
         EditText d = (EditText)findViewById(R.id.d);
 
         // Try to parse values to double
-        double k1ToDouble = tryParseToDouble(k1.getText().toString());
-        double k2ToDouble = tryParseToDouble(k2.getText().toString());
-        double k3ToDouble = tryParseToDouble(k3.getText().toString());
-        double cToDouble = tryParseToDouble(c.getText().toString());
-        double dToDouble = tryParseToDouble(d.getText().toString());
+        final double k1ToDouble = tryParseToDouble(k1.getText().toString());
+        final double k2ToDouble = tryParseToDouble(k2.getText().toString());
+        final double k3ToDouble = tryParseToDouble(k3.getText().toString());
+        final double cToDouble = tryParseToDouble(c.getText().toString());
+        final double dToDouble = tryParseToDouble(d.getText().toString());
 
         // Check if all the values has filled
-        if (k1ToDouble !=-1 && k2ToDouble != -1 && k3ToDouble != -1
-                && cToDouble != -1 && dToDouble != -1) {
+        if (k1ToDouble ==-1 && k2ToDouble == -1 && k3ToDouble == -1
+                && cToDouble == -1 && dToDouble == -1) {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "Please fill all fields", Toast.LENGTH_SHORT);
             toast.show();
             return;
         }
 
-        sendValues(k1ToDouble, k2ToDouble, k3ToDouble, cToDouble, dToDouble);
+        // Calculate the middle point of the frame.
+        midFrameCam = new PointF((float) (preview.getWidth()/2.0), (float) (preview.getHeight()/2.0));
+
+        // Repeating send values to serial every 1000 millis.
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!shouldStop) {
+                    sendValues(k1ToDouble, k2ToDouble, k3ToDouble, cToDouble, dToDouble);
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        }, 0);  //the time is in miliseconds
     }
 
     public void sendValues(double k1,double k2,double k3, double c, double d) {
         double a, b, l, e;
+
+        // Get the middle point of the rectangle face and the size of this rectangle.
+        midRectFace = faceDetectionProcessor.getMidRect();
+        L = faceDetectionProcessor.getArea();
+
+        // If the size of the rectangle is 0 - the face did'nt recognized yet.
+        if (L == 0) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "face did'nt recognized yet", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        // Calculate the Absolute values of deltaX and deltaY.
         deltaX = midFrameCam.x - midRectFace.x;
         if (deltaX < 0) {
             deltaX = -deltaX;
@@ -234,32 +208,28 @@ public class MainActivity extends AppCompatActivity {
             deltaY = -deltaY;
         }
 
-        while (true) {
-            // Check if we have this values
-            if (deltaX == -1 || deltaY == -1 || L == -1) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "face did'nt recognized yet", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-
-            // Calculate values and send them to serial
-            a = deltaX * k1 * c;
-            b = deltaY * k2;
-            l = (L - d) * k3;
-            e = (deltaX * k1) * (1 - c);
-            String s1 = "cw," + a + ",", s2 = "r," + e + ",", s3 = "fw," + l + ",", s4 = "U," + b + ",";
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    s1 + " " + s2 + " " + s3 + " " + s4, Toast.LENGTH_SHORT);
-            toast.show();
-            try {
-                port.write(s1.getBytes(), 10);
-                port.write(s2.getBytes(), 10);
-                port.write(s3.getBytes(), 10);
-                port.write(s4.getBytes(), 10);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+        // Calculate values and send them to serial
+        a = roundAvoid(deltaX * k1 * c,2);
+        b = roundAvoid(deltaY * k2,2);
+        l = roundAvoid((L - d) * k3,2);
+        e = roundAvoid((deltaX * k1) * (1 - c),2);
+        String s1 = "cw," + a + ",", s2 = "r," + e + ",", s3 = "fw," + l + ",", s4 = "U," + b + ",";
+        Toast toast = Toast.makeText(getApplicationContext(),
+                s1 + " " + s2 + " " + s3 + " " + s4, Toast.LENGTH_SHORT);
+        toast.show();
+        try {
+            port.write(s1.getBytes(), 10);
+            port.write(s2.getBytes(), 10);
+            port.write(s3.getBytes(), 10);
+            port.write(s4.getBytes(), 10);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+    }
+
+    public static double roundAvoid(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
     }
 
     public void OnClickConnent(View view) {
@@ -277,31 +247,34 @@ public class MainActivity extends AppCompatActivity {
         manager.requestPermission(driver.getDevice(), permissionIntent);
         UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
         if (connection == null) {
-            // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
             return;
         }
 
-        port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+        port = driver.getPorts().get(0);
         try {
             port.open(connection);
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
-            port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1,
+                    UsbSerialPort.PARITY_NONE);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
         Toast toast = Toast.makeText(getApplicationContext(),
                 "connected successfully to serial", Toast.LENGTH_SHORT);
         toast.show();
 
+        // Enable the start button and update shouldStop to false.
+        startButton = findViewById(R.id.send);
         startButton.setEnabled(true);
+        shouldStop = false;
     }
 
     public void OnClickDisonnent(View view) {
+        // Close the connection.
         try {
             port.close();
         } catch (IOException e) {
@@ -312,16 +285,79 @@ public class MainActivity extends AppCompatActivity {
                 "disconnected successfully from serial", Toast.LENGTH_SHORT);
         toast.show();
 
+        // Disable the start button and update shouldStop to true.
         startButton.setEnabled(false);
+        shouldStop = true;
     }
 
     public void OnClickChangeCam(View view) {
         if (changeButton.getText().equals("front cam")) {
-            cameraView.setFacing(CameraKit.Constants.FACING_FRONT);
+            cameraSource.setFacing(CameraSource.CAMERA_FACING_FRONT);
             changeButton.setText(R.string.back);
         } else {
-            cameraView.setFacing(CameraKit.Constants.FACING_BACK);
+            cameraSource.setFacing(CameraSource.CAMERA_FACING_BACK);
             changeButton.setText(R.string.front);
         }
+        preview.stop();
+        startCameraSource();
+    }
+
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info =
+                    this.getPackageManager()
+                            .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            String[] ps = info.requestedPermissions;
+            if (ps != null && ps.length > 0) {
+                return ps;
+            } else {
+                return new String[0];
+            }
+        } catch (Exception e) {
+            return new String[0];
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void getRuntimePermissions() {
+        List<String> allNeededPermissions = new ArrayList<>();
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                allNeededPermissions.add(permission);
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        Log.i(TAG, "Permission granted!");
+        if (allPermissionsGranted()) {
+            createCameraSource();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private static boolean isPermissionGranted(Context context, String permission) {
+        if (ContextCompat.checkSelfPermission(context, permission)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission granted: " + permission);
+            return true;
+        }
+        Log.i(TAG, "Permission NOT granted: " + permission);
+        return false;
     }
 }
